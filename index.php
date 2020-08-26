6c55845575903b268f92b89c1b8f4fc10fe21584
Dani Pragustia <danipragustia@disroot.org>
2020-08-26 18:13:38 +0700

    update: fix indent and optimize the script

--- index.php
+++ index.php
@@ -4,392 +4,370 @@ declare(strict_types=1);
 error_reporting(0);
 
 function cache_path(string $id) : string {
-	if (!file_exists('_cache')) {
-		mkdir('_cache', 0777);
-	}
+    if (!file_exists('_cache')) {
+	mkdir('_cache', 0777);
+    }
 
-	if (strlen($id) == 64) {
-		return '_cache/' . $id;
-	} else {
-		return '_cache/' . hash('sha256',$id, false);
-	}
+    if (strlen($id) == 64) {
+	return '_cache/' . $id;
+    } else {
+	return '_cache/' . hash('sha256',$id, false);
+    }
 }
 
 function read_data(string $id) {
-	$fpath = cache_path($id);
-	$fhandle = fopen($fpath,'r');
-	if ($fhandle && filesize($fpath) > 0) {
-		$content = fread($fhandle,filesize($fpath));
-		fclose($fhandle);
-		return json_decode($content,true);
-	} else {
-		return null;
-	}
+    $fpath = cache_path($id);
+    $fhandle = fopen($fpath,'r');
+    if ($fhandle && filesize($fpath) > 0) {
+	$content = fread($fhandle,filesize($fpath));
+	fclose($fhandle);
+	return json_decode($content,true);
+    } else {
+	return null;
+    }
 }
 
 function write_data(string $id) {
-	$driveId = $id;
-	if (strlen($driveId) == 64) {
-		if ($fdata = read_data($id)) {
-			if ($fdata['id']) {
-				$driveId = encryption('decrypt', $fdata['id']);
-			}
-		}
+    $driveId = $id;
+    if (strlen($driveId) == 64) {
+	if ($fdata = read_data($id)) {
+	    if ($fdata['id']) {
+		$driveId = enc('decrypt', $fdata['id']);
+	    }
 	}
-	$fpath = cache_path($driveId);
-	if ($fhandle = fopen($fpath,'w')) {
-		
-		$sources_list = array();
-		$ar_list = array();
-		$cookies = '';
+    }
+    $fpath = cache_path($driveId);
+    if ($fhandle = fopen($fpath,'w')) {
+	
+	$sources_list = array();
+	$ar_list = array();
+	$cookies = '';
 
-		// Check whenever file was available or not
-		$ch = curl_init('https://drive.google.com/get_video_info?docid=' . $driveId);
-		curl_setopt_array($ch,array(
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_RETURNTRANSFER => 1
-		));
-		$x = curl_exec($ch);
-		parse_str($x,$x);
-		if ($x['status'] == 'fail') {
-			curl_close($ch);
-			fclose($fhandle);
-			return null;
-		}
-		curl_close($ch);
-		
-		// Fetch Google Drive File
-		$ch = curl_init('https://drive.google.com/get_video_info?docid=' . $driveId);
-		curl_setopt_array($ch,array(
-			CURLOPT_FOLLOWLOCATION => true,
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_HEADER => 1
-		));
-		$result = curl_exec($ch);
-		curl_close($ch);
+	// Check whenever file was available or not
+	$ch = curl_init('https://drive.google.com/get_video_info?docid=' . $driveId);
+	curl_setopt_array($ch,array(
+	    CURLOPT_FOLLOWLOCATION => true,
+	    CURLOPT_RETURNTRANSFER => 1
+	));
+	$x = curl_exec($ch);
+	parse_str($x,$x);
+	if ($x['status'] == 'fail') {
+	    curl_close($ch);
+	    fclose($fhandle);
+	    return null;
+	}
+	curl_close($ch);
+	
+	// Fetch Google Drive File
+	$ch = curl_init('https://drive.google.com/get_video_info?docid=' . $driveId);
+	curl_setopt_array($ch,array(
+	    CURLOPT_FOLLOWLOCATION => true,
+	    CURLOPT_RETURNTRANSFER => 1,
+	    CURLOPT_HEADER => 1
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
 
-		// Get Cookies
-		preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
-		$cookies = array();
-		foreach($matches[1] as $item) {
-			parse_str($item, $cookie);
-			$cookies = array_merge($cookies, $cookie);
-		}
-		
-		// Parse Resolution
-		parse_str($result,$data);
-		$sources = explode(',',$data['fmt_stream_map']);
-		$fname = $data['title'];
-		foreach($sources as $source){
-			
-			switch ((int)substr($source, 0, 2)) {
-				case 18:
-					$resolution = '360p';
-					break;
-				case 59:
-					$resolution = '480p';
-					break;
-				case 22:
-					$resolution = '720p';
-					break;
-				case 37:
-					$resolution = '1080p';
-					break;
-			}
-			
-			$x = substr($source, strpos($source, "|") + 1);
-			
-			// Get Content-Length of sources
-			$curl = curl_init();
-			curl_setopt_array($curl, array(
-				CURLOPT_URL => substr($source, strpos($source, "|") + 1),
-				CURLOPT_HEADER => true,
-				CURLOPT_CONNECTTIMEOUT => 0,
-				CURLOPT_TIMEOUT => 1000,
-				CURLOPT_FRESH_CONNECT => true,
-				CURLOPT_SSL_VERIFYPEER => 0,
-				CURLOPT_NOBODY => true,
-				CURLOPT_VERBOSE => 1,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_FOLLOWLOCATION => true,
-				CURLOPT_HTTPHEADER => array(
-					'Connection: keep-alive',
-					'Cookie: DRIVE_STREAM=' . $cookies['DRIVE_STREAM']
-				)
-			));
-			
-			curl_exec($curl);
-			$content_length = curl_getinfo($curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-			curl_close($curl);
-			
-			array_push($sources_list, array(
-				'resolution' => $resolution,
-				'src' => $x,
-				'content-length' => $content_length)
-			);
-			
-			array_push($ar_list, $resolution);
-			
-		}
-		
-		// Get thumbnail Image
-		$ch = curl_init('https://drive.google.com/thumbnail?authuser=0&sz=w9999&id=' . $driveId);
-		curl_setopt($ch, CURLOPT_HEADER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		if (preg_match('~Location: (.*)~i', $result, $match)) {
-			$thumbnail = trim($match[1]);
-		} else {
-			$thumbnail = '';
-		}
-		
-		// Write to file
-		fwrite($fhandle, json_encode(array(
-			'thumbnail' => $thumbnail,
-			'cookies' => $cookies,
-			'sources' => $sources_list,
-			'id' => encryption('encrypt', $driveId)
-		)));
-		fclose($fhandle);
-		return array(
-			'status' => 200,
-			'hash' => hash('sha256', $driveId, false),
-			'sources' => $ar_list
-		); // Serve as JSON
-		
+	// Get Cookies
+	preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+	$cookies = array();
+	foreach($matches[1] as $item) {
+	    parse_str($item, $cookie);
+	    $cookies = array_merge($cookies, $cookie);
+	}
+	
+	// Parse Resolution
+	parse_str($result,$data);
+	$sources = explode(',',$data['fmt_stream_map']);
+	$fname = $data['title'];
+	foreach($sources as $source){
+	    
+	    switch ((int)substr($source, 0, 2)) {
+		case 18:
+		    $resolution = '360p';
+		    break;
+		case 59:
+		    $resolution = '480p';
+		    break;
+		case 22:
+		    $resolution = '720p';
+		    break;
+		case 37:
+		    $resolution = '1080p';
+		    break;
+	    }
+	    
+	    $x = substr($source, strpos($source, "|") + 1);
+	    
+	    // Get Content-Length of sources
+	    $curl = curl_init();
+	    curl_setopt_array($curl, array(
+		CURLOPT_URL => substr($source, strpos($source, "|") + 1),
+		CURLOPT_HEADER => true,
+		CURLOPT_CONNECTTIMEOUT => 0,
+		CURLOPT_TIMEOUT => 1000,
+		CURLOPT_FRESH_CONNECT => true,
+		CURLOPT_SSL_VERIFYPEER => 0,
+		CURLOPT_NOBODY => true,
+		CURLOPT_VERBOSE => 1,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTPHEADER => array(
+		    'Connection: keep-alive',
+		    'Cookie: DRIVE_STREAM=' . $cookies['DRIVE_STREAM']
+		)
+	    ));
+	    
+	    curl_exec($curl);
+	    $content_length = curl_getinfo($curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+	    curl_close($curl);
+	    
+	    array_push($sources_list, array(
+		'resolution' => $resolution,
+		'src' => $x,
+		'content-length' => $content_length)
+	    );
+	    
+	    array_push($ar_list, $resolution);
+	    
+	}
+	
+	// Get thumbnail Image
+	$ch = curl_init('https://drive.google.com/thumbnail?authuser=0&sz=w9999&id=' . $driveId);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	if (preg_match('~Location: (.*)~i', $result, $match)) {
+	    $thumbnail = trim($match[1]);
 	} else {
-		
-		return null; // Return null
-		
+	    $thumbnail = '';
 	}
+	
+	// Write to file
+	fwrite($fhandle, json_encode(array(
+	    'thumbnail' => $thumbnail,
+	    'cookies' => $cookies,
+	    'sources' => $sources_list,
+	    'id' => enc('encrypt', $driveId)
+	)));
+	fclose($fhandle);
+	return array(
+	    'status' => 200,
+	    'hash' => hash('sha256', $driveId, false),
+	    'sources' => $ar_list
+	); // Serve as JSON
+	
+    } else {
+	
+	return null; // Return null
+	
+    }
 }
 
 function fetch_video(array $data) : int {
+    
+    $content_length = $data['content-length'];
+    $headers = array(
+	'Connection: keep-alive',
+	'Cookie: DRIVE_STREAM=' . $data['cookie']['DRIVE_STREAM']
+    );
+    
+    if (isset($_SERVER['HTTP_RANGE'])) {
 	
-	$content_length = $data['content-length'];
-	$headers = array(
-			'Connection: keep-alive',
-			'Cookie: DRIVE_STREAM=' . $data['cookie']['DRIVE_STREAM']
-	);
+	$http = 1;
+	preg_match('/bytes=(\d+)-(\d+)?/', $_SERVER['HTTP_RANGE'], $range);
+	$initial = intval($range[1]);
+	$final = $content_length - $initial - 1;
+	array_push($headers,'Range: bytes=' . $initial . '-' . ($initial + $final));
 	
-	if (isset($_SERVER['HTTP_RANGE'])) {
-		
-		$http = 1;
-		preg_match('/bytes=(\d+)-(\d+)?/', $_SERVER['HTTP_RANGE'], $range);
-		$initial = intval($range[1]);
-		$final = $content_length - $initial - 1;
-		array_push($headers,'Range: bytes=' . $initial . '-' . ($initial + $final));
-		
-	} else {
-		
-		$http = 0;
-		
-	}
+    } else {
 	
-	if ($http == 1) {
-		
-		header('HTTP/1.1 206 Partial Content');
-		header('Accept-Ranges: bytes'); 
-		header('Content-Range: bytes ' . $initial . '-' . ($initial + $final) . '/' . $data['content-length']);
-		
-	} else {
-		
-		header('Accept-Ranges: bytes'); 
-		
-	}
+	$http = 0;
 	
-	$ch = curl_init();
+    }
+    
+    if ($http == 1) {
 	
-	curl_setopt_array($ch, array(
-		CURLOPT_URL => $data['src'],
-		CURLOPT_CONNECTTIMEOUT => 0,
-		CURLOPT_TIMEOUT => 1000,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_FOLLOWLOCATION => true,
-		CURLOPT_FRESH_CONNECT => true,
-		CURLOPT_HTTPHEADER => $headers
-	));
+	header('HTTP/1.1 206 Partial Content');
+	header('Accept-Ranges: bytes'); 
+	header('Content-Range: bytes ' . $initial . '-' . ($initial + $final) . '/' . $data['content-length']);
 	
-	curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($curl, $body) {
-		echo $body;
-		return strlen($body);
-	});
+    } else {
 	
-	header('Content-Type: video/mp4');
-	header('Content-length: ' . $content_length);
+	header('Accept-Ranges: bytes'); 
 	
-	curl_exec($ch);
+    }
+    
+    $ch = curl_init();
+    
+    curl_setopt_array($ch, array(
+	CURLOPT_URL => $data['src'],
+	CURLOPT_CONNECTTIMEOUT => 0,
+	CURLOPT_TIMEOUT => 1000,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_FRESH_CONNECT => true,
+	CURLOPT_HTTPHEADER => $headers
+    ));
+    
+    curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($curl, $body) {
+	echo $body;
+	return strlen($body);
+    });
+    
+    header('Content-Type: video/mp4');
+    header('Content-length: ' . $content_length);
+    
+    curl_exec($ch);
 
 }
 
 function stream($fdata) {
-	if (is_array($fdata)) { // Check whenver data on file was array
-			
-		$reso = $_GET['stream'];
-			
-		if ($reso == 'thumbnail') {
-			
-			header('Location:' . $fdata['thumbnail']);
-		
-		} else {
+    if (is_array($fdata)) { // Check whenver data on file was array
+	
+	$reso = $_GET['stream'];
+	
+	if ($reso == 'thumbnail') {
+	    
+	    header('Location:' . $fdata['thumbnail']);
+	    
+	} else {
 
-			foreach($fdata['sources'] as $x) {
-				if ($x['resolution'] == $_GET['stream']) {
-					fetch_video(array(
-						'content-length' => $x['content-length'],
-						'src' => $x['src'],
-						'cookie' => $fdata['cookies']
-					));
-					break;
-				}
-			}
-
+	    foreach($fdata['sources'] as $x) {
+		if ($x['resolution'] == $_GET['stream']) {
+		    fetch_video(array(
+			'content-length' => $x['content-length'],
+			'src' => $x['src'],
+			'cookie' => $fdata['cookies']
+		    ));
+		    break;
 		}
-		
-	} else { // If not remove it and tell file was corrupt
-	
-		unlink(cache_path($_GET['id']));
-		header('Content-Type: application/json');
-		die(json_encode(array(
-			'status' => 413,
-			'error' => 'File was corrupt, please re-generate file.'
-		)));
-	
+	    }
+
 	}
+	
+    } else { // If not remove it and tell file was corrupt
+	
+	unlink(cache_path($_GET['id']));
+	header('Content-Type: application/json');
+	die(json_encode(array(
+	    'status' => 413,
+	    'error' => 'File was corrupt, please re-generate file.'
+	)));
+	
+    }
 }
 
-/**
- * PHP encrypt and decrypt
- *
- * Simple method to encrypt or decrypt a plain text string initialization
- * vector(IV) has to be the same when encrypting and decrypting in PHP 5.4.9.
- *
- * @link http://naveensnayak.wordpress.com/2013/03/12/simple-php-encrypt-and-decrypt/
- * 
- * Reference: https://jonlabelle.com/snippets/view/php/php-encrypt-and-decrypt
- *
- * @param string $action Acceptable values are `encrypt` or `decrypt`.
- * @param string $string The string value to encrypt or decrypt.
- * @return string
- */
-function encryption($action, $string)
-{
-  $output = false;
- 
-  $encrypt_method = "AES-256-CBC";
-  $secret_key = 'This is my secret key';
-  $secret_iv = 'This is my secret iv';
- 
-  // hash
-  $key = hash('sha256', $secret_key);
- 
-  // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a
-  // warning
-  $iv = substr(hash('sha256', $secret_iv), 0, 16);
- 
-  if ($action == 'encrypt') {
-    $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-    $output = base64_encode($output);
-  } else {
-    if ($action == 'decrypt') {
-      $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+function enc($action, $string) {
+    $output = false;
+    
+    $encrypt_method = "AES-256-CBC";
+    $secret_key = 'This is my secret key';
+    $secret_iv = 'This is my secret iv';
+    
+    // hash
+    $key = hash('sha256', $secret_key);
+    
+    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a
+    // warning
+    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+    
+    if ($action == 'encrypt') {
+	$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+	$output = base64_encode($output);
+    } else {
+	if ($action == 'decrypt') {
+	    $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+	}
     }
-  }
- 
-  return $output;
+    
+    return $output;
 }
 
 if (isset($_GET['id'])) {
+    
+    $fdata = read_data($_GET['id']);
+    
+    if (isset($_GET['stream'])) {
 	
-	$fdata = read_data($_GET['id']);
-	
-	if (isset($_GET['stream'])) {
+	if ($fdata !== null) {
+
+	    /*
+	       Set default cached time out for 15 minutes
+	       We do this in case that the video is still processing and has only just 1 resolution, eg. 360p
+	       With 15 minutes we can update the resolution of the videos after being processed
+	    */
+	    if (time()-filemtime(cache_path($_GET['id'])) > (count($fdata['sources']) > 1 ? 3 * 3600 : 900)) { // Check cached timeout
 		
-		if ($fdata !== null) {
+		$fres = write_data($_GET['id']);
+		
+		if ($fres !== null) {
+		    
+		    stream($fdata);
 
-			// Set default cached time out for 15 minutes
-			// We do this in case that the video is still processing
-			// and has only just 1 resolution, eg. 360p
-			// With 15 minutes we can update the resolution of the videos
-			// after being processed
-			$timeout = 900;
-
-			// If sources length is more than 1, set cached time out for 3 hours
-			if (count($fdata['sources']) > 1) {
-				$timeout = 3 * 3600;
-			}
-
-			if (time()-filemtime(cache_path($_GET['id'])) > $timeout) { // Check cached time out
-				
-				$fres = write_data($_GET['id']);
-				
-				if ($fres !== null) {
-					
-					stream($fdata);
-
-				} else {
-					header('Content-Type: application/json');
-					die(json_encode(array(
-						'status' => 412,
-						'error' => 'Failed write data'
-					)));
-				}
-			
-			} else {
-				
-				stream($fdata);
-				
-			}
-			
-		} else { // If not cache file was missing or expired
-			
-			header('Content-Type: application/json');
-			die(json_encode(array(
-				'status' => 414,
-				'error' => 'Invalid file.'
-			)));
-			
+		} else {
+		    header('Content-Type: application/json');
+		    die(json_encode(array(
+			'status' => 412,
+			'error' => 'Failed write data'
+		    )));
 		}
 		
-	} else {
-
-		if (strlen($_GET['id']) < 64) {
-			if ($fdata !== null) { // Check whenever data was created before
-				header('Content-Type: application/json');
-				$ar_list = array();
-				
-				foreach($fdata['sources'] as $x) {
-					array_push($ar_list,$x['resolution']);
-				}
-				
-				echo json_encode(array(
-					'status' => 200,
-					'hash' => hash('sha256', $_GET['id'], false),
-					'sources' => $ar_list
-				)); // Server as JSON
-				
-			} else {
-			
-				$fres = write_data($_GET['id']); // Write it to file
-				if ($fres !== null) {
-					header('Content-Type: application/json');
-					echo json_encode($fres);  // Server as JSON
-				} else {
-					header('Content-Type: application/json');
-					die(json_encode(array(
-						'status' => 412,
-						'error' => 'Failed write data.'
-					)));
-				}
-			
-			}
+	    } else {
 		
-		}
-
+		stream($fdata);
+		
+	    }
+	    
+	} else { // If not cache file was missing or expired
+	    
+	    header('Content-Type: application/json');
+	    die(json_encode(array(
+		'status' => 414,
+		'error' => 'Invalid file.'
+	    )));
+	    
 	}
 	
+    } else {
+
+	if (strlen($_GET['id']) < 64) {
+	    if ($fdata !== null) { // Check whenever data was created before
+		header('Content-Type: application/json');
+		$ar_list = array();
+		
+		foreach($fdata['sources'] as $x) {
+		    array_push($ar_list,$x['resolution']);
+		}
+		
+		echo json_encode(array(
+		    'status' => 200,
+		    'hash' => hash('sha256', $_GET['id'], false),
+		    'sources' => $ar_list
+		)); // Server as JSON
+		
+	    } else {
+		
+		$fres = write_data($_GET['id']); // Write it to file
+		if ($fres !== null) {
+		    header('Content-Type: application/json');
+		    echo json_encode($fres);  // Server as JSON
+		} else {
+		    header('Content-Type: application/json');
+		    die(json_encode(array(
+			'status' => 412,
+			'error' => 'Failed write data.'
+		    )));
+		}
+		
+	    }
+	    
+	}
+
+    }
+    
 }
 
 ?>
